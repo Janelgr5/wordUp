2594fe161c5ec66f796e8d1a84a23a0f4491a4f5
Janel Grant <janelgr5@gmail.com>
2019-11-18 16:37:27 -0500

    [express] error handling 500 errors

--- server/app.js
+++ server/app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const morgan = require('morgan');
+const path = require('path');
 
 const app = express();
 app.use(morgan('dev'));
@@ -10,4 +11,10 @@ express.json();
 
 app.get('*', function(req, res) {
 	res.sendFile(path.join(__dirname, '../index.html'));
+});
+
+app.use(function(err, req, res, next) {
+	console.log('Error: ', err);
+	console.log(err.stack);
+	res.status(err.status || 500).send(err.message || 'Internal server error.');
 });
