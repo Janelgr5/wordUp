2823df2e7b2ade76dda47044a7448c6f208aa6e4
Janel Grant <janelgr5@gmail.com>
2019-11-19 16:58:40 -0500

    [sequelize] define user model - initial

--- server/db/models/User.js
+++ server/db/models/User.js
@@ -1,4 +1,32 @@
 const Sequelize = require('sequelize');
 const db = require('../index');
 
-module.exports = db.define('user', {})
+module.exports = db.define('user', {
+  firstName: {
+    type: Sequelize.STRING
+  },
+  lastName: {
+    type: Sequelize.STRING
+  },
+  email: {
+    type: Sequelize.STRING
+  },
+  username: {
+    type: Sequelize.STRING
+  },
+  password: {
+    type: Sequelize.STRING
+  },
+  age: {
+    type: Sequelize.INTEGER
+  },
+  grade: {
+    type: Sequelize.STRING
+  },
+  district: {
+    type: Sequelize.STRING
+  },
+  school: {
+    type: Sequelize.STRING
+  }
+})
