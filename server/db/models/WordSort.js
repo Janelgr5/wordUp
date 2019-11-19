ff03306cae461bca660d863c56bc9c32bda7dcda
Janel Grant <janelgr5@gmail.com>
2019-11-19 17:27:23 -0500

    [sequelize] require models in server/db/models/index.js

--- server/db/models/WordSort.js
+++ server/db/models/WordSort.js
@@ -1,4 +1,26 @@
 const Sequelize = require('sequelize');
 const db = require('../index');
 
-module.exports = db.define('wordSort', {})
+module.exports = db.define('wordSort', {
+  name: {
+    type: Sequelize.STRING
+  },
+  spellingStage: {
+    type: Sequelize.STRING
+  },
+  feature: {
+    type: Sequelize.STRING
+  },
+  sortNumber: {
+    type: Sequelize.INTEGER
+  },
+  patterns: {
+    type: Sequelize.ARRAY(Sequelize.STRING)
+  },
+  words: {
+    type: Sequelize.ARRAY(Sequelize.STRING)
+  }
+})
+
+// for emergent/letter naming stage patterns or words can be pictures or a mix of words and pictures
+// This model may need to be stored in a noSQL database?
--- server/db/models/index.js
+++ server/db/models/index.js
@@ -1,6 +1,16 @@
 const Sequelize = require('sequelize');
 const db = require('../index');
+const User = require('./user');
+const InventoryResult = require('./inventoryResult');
+const SpellingInventory = require('./spellingInventory');
+const WordSort = require('./wordSort');
 
 // ADD MODEL ASSOCIATIONS HERE
 
-module.exports = db;
+module.exports = {
+  db,
+  User,
+  InventoryResult,
+  SpellingInventory,
+  WordSort
+}
